9f30746a0082cd7e95926d64287c3d22e9c34861
Obi-Juan <JPalomino09@gmail.com>
2019-10-19 16:50:16 -0700

    mapping out firebase

--- client/src/components/SignIn/firebase.js
+++ client/src/components/SignIn/firebase.js
@@ -1,6 +1,6 @@
-const firebase = require("firebase");
-const firebaseui = require("firebaseui");
-const firebaseConfig = {
+import * as firebase from "firebase";
+
+let config = {
     apiKey: "AIzaSyB5LyyY05yJI7NYVfjlVvdYIMd0rZ-HCrs",
     authDomain: "careconnect-eefc3.firebaseapp.com",
     databaseURL: "https://careconnect-eefc3.firebaseio.com",
@@ -10,13 +10,6 @@ const firebaseConfig = {
     appId: "1:363014939249:web:d307b3b6b44f7c93a171d0",
     measurementId: "G-8LN4MSC1JW",
 };
+firebase.initializeApp(config);
 
-{
-    /* // Your web app's Firebase configuration */
-}
-
-{
-    /* // Initialize Firebase */
-}
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
+export default firebase;
