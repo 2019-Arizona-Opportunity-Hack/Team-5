adff98707836ed0e9243df0e212b019a72217cec
Brandon Burrus <brandon@burrus.io>
2019-10-20 03:25:30 -0700

    (feat) Added user and event querying

--- server/graphql/index.js
+++ server/graphql/index.js
@@ -1,10 +1,14 @@
 const fs = require("fs");
 const path = require("path");
+const config = require("../config");
 
 module.exports = {
     typeDefs: fs.readFileSync(path.join(__dirname, "./schema.graphql"), "utf-8"),
     resolvers: require("./resolvers"),
     context: require("./context"),
-    playground: true,
+    playground: !config.server.isProduction,
     introspection: true,
+    cacheControl: {
+        defaultMaxAge: config.server.isProduction ? 1800 : 0,
+    },
 };
